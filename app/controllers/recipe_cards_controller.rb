db41314f6b8a10cebf3804cab3455d3ad3ee8fba
Matt McAlister <matt.mcalister93@gmail.com>
2018-01-01 15:24:26 -0500

    added favorites functionality

--- app/controllers/recipe_cards_controller.rb
+++ app/controllers/recipe_cards_controller.rb
@@ -25,7 +25,9 @@ class RecipeCardsController < ApplicationController
     if !@recipe_card.update(recipe_card_params)
       flash[:error] = @recipe_card.errors.full_messages
     end
-    if params[:source] == "user_path(current_user)"
+    if params[:source] == "recipe_path(@recipe)"
+      redirect_to recipe_path(@recipe_card.recipe_id)
+    elsif params[:source] == "user_path(current_user)"
       redirect_to user_path(current_user)
     else
       redirect_to method(params[:source]).call
